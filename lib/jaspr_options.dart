59e32b319c357d115b2deb194c0f96f194cafe50
Nikolaos Giannakis <nicko.fox155@gmail.com>
2025-06-12 17:19:36 +0200

    modifying the dark theme

--- lib/jaspr_options.dart
+++ lib/jaspr_options.dart
@@ -7,11 +7,12 @@
 import 'package:jaspr/jaspr.dart';
 import 'package:jaspr_site/components/counter.dart' as prefix0;
 import 'package:jaspr_site/components/header.dart' as prefix1;
-import 'package:jaspr_site/constants/theme.dart' as prefix2;
-import 'package:jaspr_site/pages/about.dart' as prefix3;
-import 'package:jaspr_site/pages/contact.dart' as prefix4;
-import 'package:jaspr_site/pages/home.dart' as prefix5;
-import 'package:jaspr_site/app.dart' as prefix6;
+import 'package:jaspr_site/components/theme_toggler.dart' as prefix2;
+import 'package:jaspr_site/constants/theme.dart' as prefix3;
+import 'package:jaspr_site/pages/about.dart' as prefix4;
+import 'package:jaspr_site/pages/contact.dart' as prefix5;
+import 'package:jaspr_site/pages/home.dart' as prefix6;
+import 'package:jaspr_site/app.dart' as prefix7;
 
 /// Default [JasprOptions] for use with your jaspr project.
 ///
@@ -31,18 +32,21 @@ import 'package:jaspr_site/app.dart' as prefix6;
 /// ```
 JasprOptions get defaultJasprOptions => JasprOptions(
   clients: {
-    prefix3.About: ClientTarget<prefix3.About>('pages/about'),
+    prefix1.Header: ClientTarget<prefix1.Header>('components/header'),
 
-    prefix4.Contact: ClientTarget<prefix4.Contact>('pages/contact'),
+    prefix4.About: ClientTarget<prefix4.About>('pages/about'),
 
-    prefix5.Home: ClientTarget<prefix5.Home>('pages/home'),
+    prefix5.Contact: ClientTarget<prefix5.Contact>('pages/contact'),
+
+    prefix6.Home: ClientTarget<prefix6.Home>('pages/home'),
   },
   styles: () => [
     ...prefix0.CounterState.styles,
     ...prefix1.Header.styles,
-    ...prefix2.ThemeProviderState.styles,
-    ...prefix3.About.styles,
-    ...prefix4.Contact.styles,
-    ...prefix6.App.styles,
+    ...prefix2.ThemeToggleState.styles,
+    ...prefix3.root,
+    ...prefix4.About.styles,
+    ...prefix5.Contact.styles,
+    ...prefix7.App.styles,
   ],
 );
